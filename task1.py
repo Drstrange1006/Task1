eea6d93526db740118176abe3cbad44bafcccac8
Drstrange1006 <Shauryaharshit23@gmail.com>
2025-05-04 03:37:35 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,13 @@
+a=5
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+full_name = first_name + " " + last_name
+print("I love u " + full_name)
+
+
+
