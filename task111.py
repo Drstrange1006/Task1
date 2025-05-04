c9885e1edf0feed1d815aa5b72e9af6469584144
Drstrange1006 <Shauryaharshit23@gmail.com>
2025-05-05 00:08:52 +0530

    Add files via upload

---
+++ task111.py
@@ -0,0 +1,15 @@
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+
+print("a + b =", a + b)
+print("a - b =", a - b)
+print("a * b =", a * b)
+print("a / b =", a / b)
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+full_name = first_name + " " + last_name
+print("I love u " + full_name)
+
+
+
